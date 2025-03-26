7ba71a7fa44c73b521858649c77e1341f1a29583
KatMelon <zhangshuo_se@163.com>
2025-03-26 18:25:32 +0800

    chore: setup basic project structure and deps -by ZHANG Shuo

---
+++ src/main/java/org/segroup50/financialtracker/Main.java
@@ -0,0 +1,11 @@
+package org.segroup50.financialtracker;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
