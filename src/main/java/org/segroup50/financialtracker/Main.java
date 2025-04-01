552364e75289bc5e830a1843129ca13c14299365
Niu Xijie <2121774069@qq.com>
2025-04-01 09:29:24 +0800

    feat: add login panels and frames, implemented relevant logic -by NIU Xijie

--- src/main/java/org/segroup50/financialtracker/Main.java
+++ src/main/java/org/segroup50/financialtracker/Main.java
@@ -1,11 +1,16 @@
 package org.segroup50.financialtracker;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
+import org.segroup50.financialtracker.view.auth.login.LoginFrame;
 
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        
+        FlatDarculaLaf.setup();
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame loginFrame = new LoginFrame();
+            loginFrame.setVisible(true);
+        });
     }
 }
